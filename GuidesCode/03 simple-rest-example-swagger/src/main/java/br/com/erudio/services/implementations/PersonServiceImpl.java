dfe8ba9d45833f7c19cfe5ab23bf3e9cef75c994
leandrocgsi <leandrocgsi@gmail.com>
2019-01-16 16:36:12 -0200

    Adding guides code

---
+++ GuidesCode/03 simple-rest-example-swagger/src/main/java/br/com/erudio/services/implementations/PersonServiceImpl.java
@@ -0,0 +1,73 @@
+package br.com.erudio.services.implementations;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.stereotype.Service;
+
+import br.com.erudio.models.Person;
+import br.com.erudio.services.PersonService;
+
+@Service
+public class PersonServiceImpl implements PersonService {
+    
+  	private static final Logger logger = LoggerFactory.getLogger(PersonServiceImpl.class);
+  
+    private final AtomicLong counter = new AtomicLong();
+
+    @Override
+    public Person create(Person person) {
+    	logger.info("Creating a person");
+        Person createdPerson = person;
+        createdPerson.setId(counter.incrementAndGet());
+  		return createdPerson;
+    }
+
+    @Override
+    public Person findById(String personId) {
+      	logger.info("Finding a person by ID");
+        Person person = new Person();
+        person.setId(Long.parseLong(personId));
+        person.setFirstName("Leandro");
+        person.setLastName("Costa");
+        person.setAddress("Uberlândia - Minas Gerais - Brasil");
+        return person;
+    }
+
+    @Override
+    public List<Person> findAll() {
+      	logger.info("Finding all persons");
+        ArrayList<Person> persons = new ArrayList<>();
+        for (int i = 0; i < 8; i++) {
+            Person person = mockPerson();
+            persons.add(person);
+        }
+        return persons;
+    }
+
+    private Person mockPerson() {
+    	long id = counter.incrementAndGet();
+    	
+        Person person = new Person();
+		person.setId(id);
+        person.setFirstName("Person Name " + id);
+        person.setLastName("Last Name " + id);
+        person.setAddress("Some Address in Brasil " + id);
+        return person;
+    }
+    
+    @Override
+    public Person update(Person person) {
+      	logger.info("Updating a person");
+    	return person;
+    }
+
+    @Override
+    public void delete(String personId) {
+		logger.info("Deleting a person");
+    }
+}
