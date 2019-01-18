4fbf2f1a176b718b4f74af0cef571a60cb1c3c37
leandrocgsi <leandrocgsi@gmail.com>
2019-01-18 16:37:17 -0200

    Adding guides code

---
+++ GuidesCode/03 simple-rest-example-swagger/src/main/java/br/com/erudio/web/controllers/GreetingController.java
@@ -0,0 +1,32 @@
+package br.com.erudio.web.controllers;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.wordnik.swagger.annotations.Api;
+import com.wordnik.swagger.annotations.ApiOperation;
+
+import br.com.erudio.models.Greeting;
+
+@Api(value = "greeting")
+@RestController
+@RequestMapping("/greeting")
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+	@ApiOperation(value = "Show Greeting Message" )
+	@ResponseStatus(HttpStatus.OK)
+    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+}
