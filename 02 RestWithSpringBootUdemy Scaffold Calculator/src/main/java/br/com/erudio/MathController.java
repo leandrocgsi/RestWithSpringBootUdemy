acf2ca97fd25bb3965befc942dd5d12391d40509
leandrocgsi <leandrocgsi@gmail.com>
2019-03-16 15:43:01 -0300

    Implementing Exception Handler

--- 02 RestWithSpringBootUdemy Scaffold Calculator/src/main/java/br/com/erudio/MathController.java
+++ 02 RestWithSpringBootUdemy Scaffold Calculator/src/main/java/br/com/erudio/MathController.java
@@ -5,13 +5,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.erudio.exception.UnsuportedMathOperationException;
+
 @RestController
 public class MathController {
 	
 	@RequestMapping(value="/sum/{numberOne}/{numberTwo}", method=RequestMethod.GET)
 	public Double sum(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
 		if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
-			throw new Exception();
+			throw new UnsuportedMathOperationException("Please set a numeric value!");
 		}
 		Double sum = convertToDouble(numberOne) + convertToDouble(numberTwo);
 		return sum;		
