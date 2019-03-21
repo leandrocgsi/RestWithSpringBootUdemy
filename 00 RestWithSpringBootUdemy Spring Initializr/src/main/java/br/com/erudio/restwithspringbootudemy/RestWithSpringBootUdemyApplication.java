c21f00a1c4ea43dd4d15a42b06cd7c51855fd5fa
leandrocgsi <leandrocgsi@gmail.com>
2019-03-20 22:41:46 -0300

    Hello SpringBoot and REST Udemy

---
+++ 00 RestWithSpringBootUdemy Spring Initializr/src/main/java/br/com/erudio/restwithspringbootudemy/RestWithSpringBootUdemyApplication.java
@@ -0,0 +1,13 @@
+package br.com.erudio.restwithspringbootudemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWithSpringBootUdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWithSpringBootUdemyApplication.class, args);
+	}
+
+}
