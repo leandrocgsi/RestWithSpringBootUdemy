057eb1d82d9d7ded4ac49b12e87545e650a95fb2
leandrocgsi <leandrocgsi@gmail.com>
2019-03-22 00:28:26 -0300

    Implementing a simple REST calculator

---
+++ 02 RestWithSpringBootUdemy Scaffold Calculator/src/main/java/br/com/erudio/MathController.java
@@ -0,0 +1,33 @@
+package br.com.erudio;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MathController {
+	
+	@RequestMapping(value="/sum/numberOne/numberTwo", method=RequestMethod.GET)
+	public Double sum(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
+		if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+			throw new Exception();
+		}
+		Double sum = convertToDouble(numberOne) + convertToDouble(numberTwo);
+		return sum;		
+	}
+
+	private Double convertToDouble(String strNumber) {
+		if (strNumber == null) return 0D;
+		String number = strNumber.replaceAll(",", ".");
+		if (isNumeric(number)) return Double.parseDouble(number);
+		return 0D;
+	}
+
+	private boolean isNumeric(String strNumber) {
+		if (strNumber == null) return false;
+		String number = strNumber.replaceAll(",", ".");
+		return number.matches("[-+]?[0-9]*\\\\.?[0-9]+");
+	}
+
+}
