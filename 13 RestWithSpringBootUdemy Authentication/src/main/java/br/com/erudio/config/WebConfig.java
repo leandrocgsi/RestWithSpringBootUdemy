5931f93246bb4a5ae658b81fa604712c502640c4
leandrocgsi <leandrocgsi@gmail.com>
2019-04-18 20:32:46 -0300

    Implementing Authenticatin With JWT

--- 13 RestWithSpringBootUdemy Authentication/src/main/java/br/com/erudio/config/WebConfig.java
+++ 13 RestWithSpringBootUdemy Authentication/src/main/java/br/com/erudio/config/WebConfig.java
@@ -27,23 +27,6 @@ public class WebConfig implements WebMvcConfigurer{
 	
 	@Override
 	public void configureContentNegotiation(ContentNegotiationConfigurer configurer ) {
-		
-		// Via EXTENSION. localhost:8080/person.x-yaml
-		/*
-		 * configurer.favorParameter(false) .ignoreAcceptHeader(false)
-		 * .defaultContentType(MediaType.APPLICATION_JSON) .mediaType("json",
-		 * MediaType.APPLICATION_JSON) .mediaType("xml", MediaType.APPLICATION_XML);
-		 */
-		
-		// Via QUERY PARAM. localhost:8080/person?mediaType=xml 
-		/*
-		 * configurer.favorPathExtension(false) .favorParameter(true)
-		 * .parameterName("mediaType") .ignoreAcceptHeader(true)
-		 * .useRegisteredExtensionsOnly(false)
-		 * .defaultContentType(MediaType.APPLICATION_JSON) .mediaType("json",
-		 * MediaType.APPLICATION_JSON) .mediaType("xml", MediaType.APPLICATION_XML);
-		 */
-		
 		configurer.favorPathExtension(false)
 		.favorParameter(false)
 		.ignoreAcceptHeader(false)
