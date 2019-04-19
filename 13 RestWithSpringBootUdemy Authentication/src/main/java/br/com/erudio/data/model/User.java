ea0649a70f8fe9ba626cbb0e10ae5219236e0b32
leandrocgsi <leandrocgsi@gmail.com>
2019-04-19 00:06:13 -0300

    Implementing Authenticatin With JWT

--- 13 RestWithSpringBootUdemy Authentication/src/main/java/br/com/erudio/data/model/User.java
+++ 13 RestWithSpringBootUdemy Authentication/src/main/java/br/com/erudio/data/model/User.java
@@ -20,7 +20,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity
-@Table(name = "user")
+@Table(name = "users")
 public class User implements UserDetails, Serializable {
 
 	private static final long serialVersionUID = 1L;
