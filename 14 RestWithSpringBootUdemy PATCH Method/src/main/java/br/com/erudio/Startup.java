43b836f09e6f9a7ae3f7964cec7dc6b5923b53e9
leandrocgsi <leandrocgsi@gmail.com>
2019-04-19 14:46:56 -0300

    Working with PATCH Method

---
+++ 14 RestWithSpringBootUdemy PATCH Method/src/main/java/br/com/erudio/Startup.java
@@ -0,0 +1,20 @@
+package br.com.erudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan
+public class Startup {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Startup.class, args);
+		
+		/*BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder(16);
+		String result = bCryptPasswordEncoder.encode("admin123");
+		System.out.println("My hash " + result);*/
+	}
+}
