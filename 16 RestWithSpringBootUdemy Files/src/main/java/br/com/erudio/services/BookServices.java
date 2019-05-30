094ca74094e77c67afb27ec00d1b1edd78748975
leandrocgsi <leandrocgsi@gmail.com>
2019-05-30 02:24:20 -0300

    Working with files

--- 16 RestWithSpringBootUdemy Files/src/main/java/br/com/erudio/services/BookServices.java
+++ 16 RestWithSpringBootUdemy Files/src/main/java/br/com/erudio/services/BookServices.java
@@ -1,8 +1,8 @@
 package br.com.erudio.services;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import br.com.erudio.converter.DozerConverter;
@@ -23,9 +23,14 @@ public class BookServices {
 		return vo;
 	}
 	
-	public List<BookVO> findAll() {
-		return DozerConverter.parseListObjects(repository.findAll(), BookVO.class);
+	public Page<BookVO> findAll(Pageable pageable) {
+		var page = repository.findAll(pageable);
+		return page.map(this::convertToBookVO);
 	}	
+	
+	private BookVO convertToBookVO(Book entity) {
+		return DozerConverter.parseObject(entity, BookVO.class);
+	}
 	
 	public BookVO findById(Long id) {
 
