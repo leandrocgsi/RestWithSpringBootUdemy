1fe8c7c6c6c67ed03535021bec609f3066d8d6c7
leandrocgsi <leandrocgsi@gmail.com>
2019-06-01 22:25:25 -0300

    Working with files

--- 16 RestWithSpringBootUdemy Files/src/main/java/br/com/erudio/Startup.java
+++ 16 RestWithSpringBootUdemy Files/src/main/java/br/com/erudio/Startup.java
@@ -3,9 +3,15 @@ package br.com.erudio;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 
+import br.com.erudio.config.FileStorageConfig;
+
 @SpringBootApplication
+@EnableConfigurationProperties({
+	FileStorageConfig.class
+})
 @EnableAutoConfiguration
 @ComponentScan
 public class Startup {
