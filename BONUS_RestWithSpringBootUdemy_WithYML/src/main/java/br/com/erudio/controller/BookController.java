05fd0c402ad7e644771e730a60e71b3cf72e93ce
leandrocgsi <leandrocgsi@gmail.com>
2020-12-11 15:31:08 -0300

    Replacing application.properties to application.yml

---
+++ BONUS_RestWithSpringBootUdemy_WithYML/src/main/java/br/com/erudio/controller/BookController.java
@@ -0,0 +1,100 @@
+package br.com.erudio.controller;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.PagedResources;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.erudio.data.vo.v1.BookVO;
+import br.com.erudio.services.BookServices;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+@Api(tags = "BookEndpoint") 
+@RestController
+@RequestMapping("/api/book/v1")
+public class BookController {
+	
+	@Autowired
+	private BookServices service;
+	
+	@Autowired
+	private PagedResourcesAssembler<BookVO> assembler;
+	
+	@ApiOperation(value = "Find all books" )
+	@GetMapping(produces = { "application/json", "application/xml", "application/x-yaml" })
+	public ResponseEntity<?> findAll(
+			@RequestParam(value="page", defaultValue = "0") int page,
+			@RequestParam(value="limit", defaultValue = "12") int limit,
+			@RequestParam(value="direction", defaultValue = "asc") String direction) {
+		
+
+		var sortDirection = "desc".equalsIgnoreCase(direction) ? Direction.DESC : Direction.ASC;
+		
+		Pageable pageable = PageRequest.of(page, limit, Sort.by(sortDirection, "title"));
+		
+		Page<BookVO> books =  service.findAll(pageable);
+		books
+			.stream()
+			.forEach(p -> p.add(
+					linkTo(methodOn(BookController.class).findById(p.getKey())).withSelfRel()
+				)
+			);
+		
+		PagedResources<?> resources = assembler.toResource(books);
+		
+		return new ResponseEntity<>(resources, HttpStatus.OK);
+	}	
+	
+	@ApiOperation(value = "Find a specific book by your ID" )
+	@GetMapping(value = "/{id}", produces = { "application/json", "application/xml", "application/x-yaml" })
+	public BookVO findById(@PathVariable("id") Long id) {
+		BookVO bookVO = service.findById(id);
+		bookVO.add(linkTo(methodOn(BookController.class).findById(id)).withSelfRel());
+		return bookVO;
+	}	
+	
+	@ApiOperation(value = "Create a new book")
+	@PostMapping(produces = { "application/json", "application/xml", "application/x-yaml" }, 
+			consumes = { "application/json", "application/xml", "application/x-yaml" })
+	public BookVO create(@RequestBody BookVO book) {
+		BookVO bookVO = service.create(book);
+		bookVO.add(linkTo(methodOn(BookController.class).findById(bookVO.getKey())).withSelfRel());
+		return bookVO;
+	}
+	
+	@ApiOperation(value = "Update a specific book")
+	@PutMapping(produces = { "application/json", "application/xml", "application/x-yaml" }, 
+			consumes = { "application/json", "application/xml", "application/x-yaml" })
+	public BookVO update(@RequestBody BookVO book) {
+		BookVO bookVO = service.update(book);
+		bookVO.add(linkTo(methodOn(BookController.class).findById(bookVO.getKey())).withSelfRel());
+		return bookVO;
+	}	
+	
+	@ApiOperation(value = "Delete a specific book by your ID")
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> delete(@PathVariable("id") Long id) {
+		service.delete(id);
+		return ResponseEntity.ok().build();
+	}	
+	
+}
---
+++ BONUS_RestWithSpringBootUdemy_WithYML/src/main/java/br/com/erudio/services/BookServices.java
@@ -0,0 +1,61 @@
+package br.com.erudio.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import br.com.erudio.converter.DozerConverter;
+import br.com.erudio.data.model.Book;
+import br.com.erudio.data.vo.v1.BookVO;
+import br.com.erudio.exception.ResourceNotFoundException;
+import br.com.erudio.repository.BookRepository;
+
+@Service
+public class BookServices {
+	
+	@Autowired
+	BookRepository repository;
+		
+	public BookVO create(BookVO book) {
+		var entity = DozerConverter.parseObject(book, Book.class);
+		var vo = DozerConverter.parseObject(repository.save(entity), BookVO.class);
+		return vo;
+	}
+	
+	public Page<BookVO> findAll(Pageable pageable) {
+		var page = repository.findAll(pageable);
+		return page.map(this::convertToBookVO);
+	}	
+	
+	private BookVO convertToBookVO(Book entity) {
+		return DozerConverter.parseObject(entity, BookVO.class);
+	}
+	
+	public BookVO findById(Long id) {
+
+		var entity = repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
+		return DozerConverter.parseObject(entity, BookVO.class);
+	}
+		
+	public BookVO update(BookVO book) {
+		var entity = repository.findById(book.getKey())
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
+		
+		entity.setAuthor(book.getAuthor());
+		entity.setLaunchDate(book.getLaunchDate());
+		entity.setPrice(book.getPrice());
+		entity.setTitle(book.getTitle());
+		
+		var vo = DozerConverter.parseObject(repository.save(entity), BookVO.class);
+		return vo;
+	}	
+	
+	public void delete(Long id) {
+		Book entity = repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
+		repository.delete(entity);
+	}
+
+}
